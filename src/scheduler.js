6eee079691e0c7bb0649eb91bbb9e95814950506
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-04-14 22:03:07 -0300

    Fixes

--- src/scheduler.js
+++ src/scheduler.js
@@ -11,7 +11,12 @@ const {
   reminder_text
 } = require("../config");
 
-const twitter = new Twitter(config);
+const twitter = new Twitter({
+  consumer_key: config.consumer_key,
+  consumer_secret: config.consumer_secret,
+  access_token: config.access_token,
+  access_token_secret: config.access_token_secret
+});
 let agenda;
 
 module.exports.run = async () => {
