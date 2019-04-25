2806f7cc5bc45dfad7d3a4eb3cb40d67c662a8ce
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-04-25 17:53:32 -0300

    file download in progress. commited to make a checkpoint

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-// const { username } = require("./config");
-// const reminder = require("./src/controllers/reminder");
+const { username } = require("./config");
+const reminder = require("./src/controllers/reminder");
+const saveFile = require("./src/utils/save-to-file");
 
-// const twitter = require("./src/lib/twitter");
+const twitter = require("./src/lib/twitter");
 
-// try {
-//   const stream = twitter.stream("statuses/filter", { track: [username] });
+try {
+  const stream = twitter.stream("statuses/filter", { track: [username] });
 
-//   console.log("Listening to tweets...");
+  console.log("Listening to tweets...");
 
-//   stream.on("tweet", async tweet => {
-//     console.log("Tweet received!");
+  stream.on("tweet", async tweet => {
+    console.log("Tweet received!");
 
-//     console.log(tweet.entities.media.media_url);
-//     // reminder(tweet);
-//     // downlooad(tweet)
-//   });
-// } catch (err) {
-//   console.log("Error: " + err);
-// }
-
-// const download = require("./src/controllers/download");
-
-// download("");
+    // reminder(tweet);
+    const tweetMediaId = tweet.in_reply_to_status_id_str;
+    twitter.get("statuses/show/:id", { id: tweetMediaId }, (err, tweet) => {
+      if (err) {
+        console.log("Error on getting the tweet: " + err);
+      } else {
+        saveFile(tweet);
+      }
+    });
+  });
+} catch (err) {
+  console.log("Error: " + err);
+}
--- src/utils/save-to-file.js
+++ src/utils/save-to-file.js
@@ -2,25 +2,38 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-module.exports = tweet => {
+const { reply } = require("./twitter");
+
+module.exports = async tweet => {
   try {
-    tweet.id_str = "matheus";
+    const user = tweet.user.screen_name;
+    const tweetId = tweet.id_str;
+    const tweetMediaId = tweet.in_reply_to_status_id_str;
+    console.log(tweet.extended_entities[0].media);
+    let extension;
+    if (tweet.extended_entities[0].media.type === "photo") extension = ".jpg";
+    else if (tweet.extended_entities[0].media.type === "video")
+      extension = ".mp4";
+    else if (tweet.extended_entities[0].media.type === "animated_gif")
+      extension = ".gif";
+    console.log(extension);
+    const filename = tweet.id_str + extension;
+
     const file = fs.createWriteStream(
-      path.join(
-        __dirname,
-        "../..",
-        "downloads",
-        // tweet.id_str
-        "matheus" + ".jpg"
-      )
+      path.join(__dirname, "../..", "downloads", filename)
     );
-    http.get(
-      //tweet.entities.media.media_url,
-      "http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg",
-      response => {
-        response.pipe(file);
-      }
-    );
+    await http.get(tweet.extended_entities.media.media_url, response => {
+      response.pipe(file);
+    });
+
+    // const url = process.env.NODE_ENV
+    //   ? process.env.PRODUCTION
+    //   : process.env.DEVELOPMENT;
+
+    const replyText =
+      "Here's the link for your download: " + "localhost:8080/" + filename;
+    console.log(replyText);
+    // reply(user, tweetId, replyText);
   } catch (err) {
     console.log("Error download: " + err);
   }
